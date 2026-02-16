c5ec2b7bae0c852b2235d4ff0fd5ab608cb2bfbc
Simone Machetti <simone.machetti@epfl.ch>
2026-02-16 15:31:00 +0100

    Fixed Yosys synthesis.

--- hw/imp/1_syn_yosys/scripts/compile.tcl
+++ hw/imp/1_syn_yosys/scripts/compile.tcl
@@ -8,13 +8,27 @@
 yosys "design -reset"
 
 # -----------------------------------------------------------------------------
+# Import Yosys Slang plugin
+# -----------------------------------------------------------------------------
+yosys "plugin -i $env(YOSYS_SLANG_HOME)/bin/slang.so"
+
+# -----------------------------------------------------------------------------
 # Read SystemVerilog sources
 # -----------------------------------------------------------------------------
-yosys "read_verilog -sv $env(CODE_HOME)/ai_core/hw/src/rtl/top_level/multsigned/AdderN.sv"
-yosys "read_verilog -sv $env(CODE_HOME)/ai_core/hw/src/rtl/top_level/multsigned/Encoder.sv"
-yosys "read_verilog -sv $env(CODE_HOME)/ai_core/hw/src/rtl/top_level/multsigned/Extender.sv"
-yosys "read_verilog -sv $env(CODE_HOME)/ai_core/hw/src/rtl/top_level/multsigned/Mux9x1.sv"
-yosys "read_verilog -sv $env(CODE_HOME)/ai_core/hw/src/rtl/top_level/multsigned/Shifter1.sv"
-yosys "read_verilog -sv $env(CODE_HOME)/ai_core/hw/src/rtl/top_level/multsigned/Shifter2.sv"
-
-yosys "read_verilog -sv $env(CODE_HOME)/ai_core/hw/src/rtl/top_level/multsigned.sv"
+yosys "read_slang \
+  $env(CODE_HOME)/ai_core/hw/src/rtl/top_level/multsigned/AdderN.sv \
+  $env(CODE_HOME)/ai_core/hw/src/rtl/top_level/multsigned/Encoder.sv \
+  $env(CODE_HOME)/ai_core/hw/src/rtl/top_level/multsigned/Extender.sv \
+  $env(CODE_HOME)/ai_core/hw/src/rtl/top_level/multsigned/Mux9x1.sv \
+  $env(CODE_HOME)/ai_core/hw/src/rtl/top_level/multsigned/Shifter1.sv \
+  $env(CODE_HOME)/ai_core/hw/src/rtl/top_level/multsigned/Shifter2.sv \
+  $env(CODE_HOME)/ai_core/hw/src/rtl/top_level/full_adder.sv \
+  $env(CODE_HOME)/ai_core/hw/src/rtl/top_level/sign_extender.sv \
+  $env(CODE_HOME)/ai_core/hw/src/rtl/top_level/compressor_4_2_cell.sv \
+  $env(CODE_HOME)/ai_core/hw/src/rtl/top_level/compressor_4_2_n_bit.sv \
+  $env(CODE_HOME)/ai_core/hw/src/rtl/top_level/compressor_8_2_n_bit.sv \
+  $env(CODE_HOME)/ai_core/hw/src/rtl/top_level/compressor_24_2.sv \
+  $env(CODE_HOME)/ai_core/hw/src/rtl/top_level/multsigned.sv \
+  $env(CODE_HOME)/ai_core/hw/src/rtl/top_level/multsigned_array.sv \
+  $env(CODE_HOME)/ai_core/hw/src/rtl/baseline.sv \
+  $env(CODE_HOME)/ai_core/hw/src/rtl/baseline.sv"
