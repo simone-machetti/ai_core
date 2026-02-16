9ad159215f9a31deb3b09789ce7e47fb0c510068
Simone Machetti <simone.machetti@epfl.ch>
2026-02-16 10:41:24 +0100

    Fixed Verilator simulation.

--- hw/imp/0_sim_verilator/scripts/filelist.f
+++ hw/imp/0_sim_verilator/scripts/filelist.f
@@ -8,6 +8,16 @@ $(CODE_HOME)/ai_core/hw/src/rtl/top_level/multsigned/Extender.sv
 $(CODE_HOME)/ai_core/hw/src/rtl/top_level/multsigned/Mux9x1.sv
 $(CODE_HOME)/ai_core/hw/src/rtl/top_level/multsigned/Shifter1.sv
 $(CODE_HOME)/ai_core/hw/src/rtl/top_level/multsigned/Shifter2.sv
+
+$(CODE_HOME)/ai_core/hw/src/rtl/top_level/full_adder.sv
+$(CODE_HOME)/ai_core/hw/src/rtl/top_level/sign_extender.sv
+$(CODE_HOME)/ai_core/hw/src/rtl/top_level/compressor_4_2_cell.sv
+$(CODE_HOME)/ai_core/hw/src/rtl/top_level/compressor_4_2_n_bit.sv
+$(CODE_HOME)/ai_core/hw/src/rtl/top_level/compressor_8_2_n_bit.sv
+$(CODE_HOME)/ai_core/hw/src/rtl/top_level/compressor_24_2.sv
 $(CODE_HOME)/ai_core/hw/src/rtl/top_level/multsigned.sv
+$(CODE_HOME)/ai_core/hw/src/rtl/top_level/multsigned_array.sv
+
+$(CODE_HOME)/ai_core/hw/src/rtl/baseline.sv
 
 $(CODE_HOME)/ai_core/hw/src/tb/testbench.sv
