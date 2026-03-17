6aa665f16f261a118c24e61b25ebc1134bf006b7
Simone Machetti <simone.machetti@epfl.ch>
2026-03-17 11:03:50 +0100

    Fixed compressor_4_2.

--- rtl/compressor_4_2.sv
+++ rtl/compressor_4_2.sv
@@ -8,22 +8,27 @@
 `timescale 1 ns/1 ps
 
 module compressor_4_2 #(
-    parameter int IN_SIZE  = 14,
-    parameter int OUT_SIZE = 16
+    parameter int IN_SIZE    = 14,
+    parameter int EXTRA_BITS = 2, // Use only 1 or 2
+
+    localparam int OUT_SIZE = IN_SIZE + EXTRA_BITS
 )(
     input  logic [ IN_SIZE-1:0] in_i [4],
     output logic [OUT_SIZE-1:0] sum_o,
     output logic [OUT_SIZE-1:0] carry_o
 );
-    logic [OUT_SIZE-2:0] ext_in [4];
+
+    localparam EXT_SIZE = OUT_SIZE - 1;
+
+    logic [EXT_SIZE-1:0] ext_in [4];
+    logic [EXT_SIZE-1:0] s, c;
     logic [OUT_SIZE-1:0] cout;
-    logic [OUT_SIZE-2:0] s, c;
 
     generate
         for (genvar i = 0; i < 4; i++) begin : gen_extenders
             sign_extender #(
-                .IN_SIZE(IN_SIZE),
-                .OUT_SIZE(OUT_SIZE-1)
+                .IN_SIZE (IN_SIZE),
+                .OUT_SIZE(EXT_SIZE)
             ) sign_extender_i (
                 .in_i (in_i[i]),
                 .out_o(ext_in[i])
@@ -34,7 +39,7 @@ module compressor_4_2 #(
     assign cout[0] = 1'b0;
 
     generate
-        for (genvar i = 0; i < OUT_SIZE-1; i++) begin : gen_compressor_4_2_cell_i
+        for (genvar i = 0; i < EXT_SIZE; i++) begin : gen_compressor_4_2_cell_i
             compressor_4_2_cell compressor_4_2_cell_i (
                 .in_0_i (ext_in[0][i]),
                 .in_1_i (ext_in[1][i]),
@@ -49,6 +54,6 @@ module compressor_4_2 #(
     endgenerate
 
     assign carry_o = {c, 1'b0};
-    assign sum_o   = {s[OUT_SIZE-2], s};
+    assign sum_o   = {s[EXT_SIZE-1], s};
 
 endmodule
