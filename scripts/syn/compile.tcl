88b0e16349896b7b7f4b9f456485046d3a3bb7ce
Simone Machetti <simone.machetti@epfl.ch>
2026-04-09 12:16:05 +0200

    Updated synthesis.

--- scripts/syn/compile.tcl
+++ scripts/syn/compile.tcl
@@ -24,41 +24,7 @@ yosys "read_liberty -lib $env(TOOLS_HOME)/OpenROAD-flow-scripts/flow/platforms/a
 # -----------------------------------------------------------------------------
 # Read SystemVerilog sources
 # -----------------------------------------------------------------------------
-set rtl_files [list \
-    $env(CODE_HOME)/ai-core/rtl/ha.sv \
-    $env(CODE_HOME)/ai-core/rtl/fa.sv \
-    $env(CODE_HOME)/ai-core/rtl/ff.sv \
-    $env(CODE_HOME)/ai-core/rtl/ff_n.sv \
-    $env(CODE_HOME)/ai-core/rtl/sign_ext.sv \
-    $env(CODE_HOME)/ai-core/rtl/ext_n.sv \
-    $env(CODE_HOME)/ai-core/rtl/shifter_n.sv \
-    $env(CODE_HOME)/ai-core/rtl/add_n.sv \
-    $env(CODE_HOME)/ai-core/rtl/adder_n.sv \
-    $env(CODE_HOME)/ai-core/rtl/booth_r4_cell.sv \
-    $env(CODE_HOME)/ai-core/rtl/booth_r8_cell.sv \
-    $env(CODE_HOME)/ai-core/rtl/cpr_4_2_bit.sv \
-    $env(CODE_HOME)/ai-core/rtl/cpr_4_2.sv \
-    $env(CODE_HOME)/ai-core/rtl/cpr_n_2.sv \
-    $env(CODE_HOME)/ai-core/rtl/booth_r4.sv \
-    $env(CODE_HOME)/ai-core/rtl/booth_r8.sv \
-    $env(CODE_HOME)/ai-core/rtl/sqr_4_bit.sv \
-    $env(CODE_HOME)/ai-core/rtl/sqr_5_bit.sv \
-    $env(CODE_HOME)/ai-core/rtl/add_mult_array.sv \
-    $env(CODE_HOME)/ai-core/rtl/mult_array.sv \
-    $env(CODE_HOME)/ai-core/rtl/add_sqr_array.sv \
-    $env(CODE_HOME)/ai-core/rtl/cpr_tree.sv \
-    $env(CODE_HOME)/ai-core/rtl/bas_4x4.sv \
-    $env(CODE_HOME)/ai-core/rtl/bas_4x8.sv \
-    $env(CODE_HOME)/ai-core/rtl/win_4x4.sv \
-    $env(CODE_HOME)/ai-core/rtl/win_4x8.sv \
-    $env(CODE_HOME)/ai-core/rtl/sqr_4x4.sv \
-]
-
-set top_file "$env(CODE_HOME)/ai-core/rtl/$env(SEL_TOP_LEVEL).sv"
-
-if {[lsearch $rtl_files $top_file] == -1} {
-    lappend rtl_files $top_file
-}
+set rtl_files [lsort [glob "$env(CODE_HOME)/ai-core/rtl/*.sv"]]
 
 set g_flags ""
 if {$env(SEL_PARAMS) ne "none"} {
@@ -67,4 +33,4 @@ if {$env(SEL_PARAMS) ne "none"} {
     }
 }
 
-yosys "read_slang [join $rtl_files]$g_flags"
+yosys "read_slang [join $rtl_files] --top $env(SEL_TOP_LEVEL)$g_flags"
