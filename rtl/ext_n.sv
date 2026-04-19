0bf462d4b42da491f849be872d1b613eabd3126c
Simone Machetti <simone.machetti@epfl.ch>
2026-04-19 15:20:00 +0200

    Updated ext_n.

--- rtl/ext_n.sv
+++ rtl/ext_n.sv
@@ -7,28 +7,22 @@
 `timescale 1 ns/1 ps
 
 module ext_n #(
-    parameter int IN_SIZE  = 2,
-    parameter int IN_WIDTH = 8,
-    parameter int EXTEND   = 4
-)(
-    input  logic                       is_signed_i,
-    input  logic                       is_shift_i,
-    input  logic [       IN_WIDTH-1:0] in_i  [0:IN_SIZE-1],
-    output logic [IN_WIDTH+EXTEND-1:0] out_o [0:IN_SIZE-1]
-);
+    parameter int IN_SIZE   = 2,
+    parameter int IN_WIDTH  = 8,
+    parameter int EXTEND    = 4,
+    parameter bit IS_SIGNED = 1,
 
-    logic [IN_WIDTH+EXTEND-1:0] tmp [0:IN_SIZE-1];
+    localparam int OUT_WIDTH = IN_WIDTH + EXTEND,
+    localparam int OUT_SIZE  = IN_SIZE
+)(
+    input  logic [ IN_WIDTH-1:0] in_i  [ 0:IN_SIZE-1],
+    output logic [OUT_WIDTH-1:0] out_o [0:OUT_SIZE-1]
+);
 
     genvar i;
     generate
-        for (i = 0; i < IN_SIZE; i++) begin : gen_extend
-            assign tmp[i] = is_signed_i == 1'b1 ? {{EXTEND{in_i[i][IN_WIDTH-1]}}, in_i[i]} : {{EXTEND{1'b0}}, in_i[i]};
-        end
-    endgenerate
-
-    generate
-        for (i = 0; i < IN_SIZE; i++) begin : gen_shift
-            assign out_o[i] = is_shift_i == 1'b1 ? {tmp[i][IN_WIDTH-1:0], {EXTEND{1'b0}}} : tmp[i];
+        for (i = 0; i < IN_SIZE; i++) begin
+            assign out_o[i] = IS_SIGNED ? {{EXTEND{in_i[i][IN_WIDTH-1]}}, in_i[i]} : {{EXTEND{1'b0}}, in_i[i]};
         end
     endgenerate
 
